0da04cb642c376130f7ff2a475aa91a7fde9e3cd
Wojciech <wojciechonoszko@gmail.com>
2022-06-20 21:31:47 +0200

    Update ImageGallery.js

--- src/components/imageGallery/ImageGallery.js
+++ src/components/imageGallery/ImageGallery.js
@@ -3,32 +3,64 @@ import PropTypes from 'prop-types';
 import ImageGalleryItem from '../imageGalleryItem/ImageGalleryItem'
 import {ImageGalleryContainer} from './ImageGalleryStyles';
 
-const ImageGallery = ({onItemClick, images}) => {
+
+export default function ImageGallery({ response, toggleModal, onImageClick }) {
     return (
-        <ImageGalleryContainer
-            onClick={event => onItemClick(event.target.dataset.largeImageUrl)}
-        >
-            {images.map(image => (
-                <ImageGalleryItem
-                    key={image.id}
-                    id={image.id}
-                    imgURL={image.webformatURL}
-                    largeImageURL={image.largeImageURL}
-                />
-            ))}
+        <ImageGalleryContainer>
+        {response.map(({ id, webformatURL, tags, largeImageURL }) => (
+            <ImageGalleryItem
+              key={id}
+              id={id}
+              url={webformatURL}
+              tag={tags}
+              toggleModal={() => toggleModal()}
+              onImageClick={() => onImageClick(id, largeImageURL, tags)}
+            />
+          ))}
         </ImageGalleryContainer>
-    );
-};
+    ); 
+}
 
 ImageGallery.propTypes = {
-    onItemClick: PropTypes.func.isRequired,
-    images: PropTypes.arrayOf(
-        PropTypes.shape({
-            webformatURL: PropTypes.string.isRequired,
-            largeImageURL: PropTypes.string.isRequired,
-            id: PropTypes.number.isRequired
-        })
-    )
+    response: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        tags: PropTypes.string,
+        webformatURL: PropTypes.string,
+        largeImageURL: PropTypes.string,
+      })
+    ),
+    toggleModal: PropTypes.func.isRequired,
+    onImageClick: PropTypes.func.isRequired,
 };
 
-export default ImageGallery;
+
+// const ImageGallery = ({onItemClick, images}) => {
+//     return (
+//         <ImageGalleryContainer
+//             onClick={event => onItemClick(event.target.dataset.largeImageUrl)}
+//         >
+//             {images.map(image => (
+//                 <ImageGalleryItem
+//                     key={image.id}
+//                     id={image.id}
+//                     imgURL={image.webformatURL}
+//                     largeImageURL={image.largeImageURL}
+//                 />
+//             ))}
+//         </ImageGalleryContainer>
+//     );
+// };
+
+// ImageGallery.propTypes = {
+//     onItemClick: PropTypes.func.isRequired,
+//     images: PropTypes.arrayOf(
+//         PropTypes.shape({
+//             webformatURL: PropTypes.string.isRequired,
+//             largeImageURL: PropTypes.string.isRequired,
+//             id: PropTypes.number.isRequired
+//         })
+//     )
+// };
+
+// export default ImageGallery;
