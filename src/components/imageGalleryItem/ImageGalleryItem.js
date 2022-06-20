6043aaa6b6993f20a032937075f500212ebb4a31
Wojciech <wojciechonoszko@gmail.com>
2022-06-20 14:23:49 +0200

    update files

---
+++ src/components/imageGalleryItem/ImageGalleryItem.js
@@ -0,0 +1,24 @@
+import {ImageGalleryItemContainer} from './ImageGalleryItemStyles';
+import PropTypes from 'prop-types';
+
+const ImageGalleryItem = ({id, imgURL, largeImageURL}) => {
+    return (
+        <ImageGalleryItemContainer>
+            <img
+                src={imgURL}
+                data-large-image-url={largeImageURL}
+                key={id}
+                alt=""
+                className="ImageGalleryItem-image"
+            />
+        </ImageGalleryItemContainer>
+    );
+};
+
+ImageGalleryItem.propTypes = {
+    imgURL: PropTypes.string.isRequired,
+    largeImageURL: PropTypes.string.isRequired
+
+};
+
+export default ImageGalleryItem;
---
+++ src/components/loader/Loader.js
@@ -0,0 +1,33 @@
+// import {Audio} from 'react-loader-spinner';
+// export const MyLoader = () => {
+//     return (
+//       <Audio
+//       height="100"
+//       width="100"
+//       color='pink'
+//       ariaLabel='loading'
+//       margin-left='50px'
+//     />
+//     );
+// };
+
+//npm install --save react-spinners
+
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import { css } from "@emotion/react";
+
+const override = css`
+  position: fixed;
+  left: 50%;
+  top: 50%;
+`;
+
+export const MyLoader = () => {
+    return (
+      <ClimbingBoxLoader
+      color="purple"
+      size="50px"
+      css={override}
+    />
+    );
+};
